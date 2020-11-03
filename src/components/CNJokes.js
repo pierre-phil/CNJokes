9f891eed47225b6199ccc302ef03619e130d982b
pierre-phil <pierre-philippon@live.fr>
2020-11-03 17:32:12 +0100

    start

---
+++ src/components/CNJokes.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useReducer, useState } from "react";
+import { useToast, Spinner } from "@chakra-ui/core";
+import axios from "axios";
+
+import "../App.css";
+
+const CNJokes = () => {
+  const toast = useToast();
+
+  const [random, setRandom] = useState("");
+
+  const jokeReducer = (state, action) => {
+    switch (action.type) {
+      case "FETCH_INIT":
+        return { ...state, loading: true, error: false };
+      case "FETCH_SUCCESS":
+        return { ...state, loading: false, data: action.payload };
+      case "FETCH_FAILURE":
+        return { ...state, error: true };
+      default:
+        throw new Error(`Unhandled acion ${action.type} in jokeReducer`);
+    }
+  };
+
+  const initialState = {
+    data: "",
+    loading: false,
+    error: false,
+  };
+
+  const [state, dispatch] = useReducer(jokeReducer, initialState);
+
+  useEffect(() => {
+    const fetchJokes = async () => {
+      dispatch({ type: "FETCH_INIT" });
+      try {
+        const result = await axios({
+          method: "GET",
+          url: "https://rapidapi.p.rapidapi.com/jokes/random",
+          headers: {
+            accept: "application/json",
+            "x-rapidapi-key":
+              "fadbe4acd1msh8e4895490a6027ep1a60b5jsnc6302d5937e5",
+            "x-rapidapi-host":
+              "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+          },
+        });
+        console.log("result", result.data);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (error) {
+        dispatch({ type: "FETCH_FAILURE" });
+        console.log("fail");
+        toast({
+          title: "Sorry, we couldn't fetch your joke",
+          status: "error",
+          duration: 3000,
+        });
+      }
+    };
+    fetchJokes();
+  }, [random]);
+
+  const getRandomJoke = () => {
+    setRandom(state.data.value);
+  };
+
+  return (
+    <div className="container my-5">
+      <header>
+        <h1 className="h1">Hello Bitches</h1>
+        <img alt="Chuck Norris logo" src={state.data.icon_url}></img>
+      </header>
+      <section className="container my-5">
+        <button onClick={getRandomJoke}>Random</button>
+        <button className="mx-2">Categories</button>
+      </section>
+
+      <section className="container my-5">
+        {state.loading ? <Spinner /> : <p>{random}</p>}
+      </section>
+    </div>
+  );
+};
+
+export default CNJokes;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider, theme } from "@chakra-ui/core";
+import "focus-visible/dist/focus-visible";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme} resetCSS={true}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
